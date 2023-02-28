c408b3b3c9657682c5590ebf3fcbf474c48943ff
acronym1986 <acronym1986@gmail.com>
2023-02-28 13:07:59 +1100

    add text and div

--- dist/addText.js
+++ dist/addText.js
@@ -1,8 +1,11 @@
 export function enigmatext() {
   const paratext = `
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-  Eligendi cum facilis natus dolores corporis voluptate nam dignissimos dolor, 
-  asperiores esse, sint distinctio blanditiis quasi fugit nesciunt est rem facere vero?
+  Move beyond RPA and experience the power of APA with ENIGMA2.0, 
+  a software solution that adapts, scales, and evolves with you. 
+  Robotic Process Automation (RPA) is the use of software “robots" who perform the same way every time and don't learn from one repetition to another. 
+  AI Process Automation in Enigma first learns your business processes (acquiring information), 
+  improves them (using context and rules) and finally, 
+  performs intelligent internal self-correction (learning from successes and failures) to enable immediate action
   `
   var obj_paratext = {
       name:  'Enigma',
@@ -15,9 +18,14 @@ export function enigmatext() {
 
 export function fabriqtext() {
   const paratext = `
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-  Eligendi cum facilis natus dolores corporis voluptate nam dignissimos dolor, 
-  asperiores esse, sint distinctio blanditiis quasi fugit nesciunt est rem facere vero?
+  Presenting a brain-modeled data supremacy, 
+  which specializes in systems integration and interoperability solutions, 
+  with a focus on knitting disparate data sources, IoT devices, SaaS applications, 
+  and platforms together, so each component works together harmoniously, 
+  producing insights in real-time and delivering a capability greater than just the sum of its elements. 
+  A Hybrid Intelligence (HI) system that combines the very unique processing strengths of AI and the human brain to result in a perfect symbiosis. 
+  Think of FABRIQ as the virtual network of distributed neurons in the brain, 
+  linking historically siloed, disconnected systems to power smarter, more informed operations.
   `
   var obj_paratext = {
       name:  'Fabriq',
@@ -30,9 +38,9 @@ export function fabriqtext() {
 
 export function algoreustext() {
   const paratext = `
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-  Eligendi cum facilis natus dolores corporis voluptate nam dignissimos dolor, 
-  asperiores esse, sint distinctio blanditiis quasi fugit nesciunt est rem facere vero?
+  Presenting a Machine Learning Spawner that creates, trains, predicts, deploys, manages, 
+  and scales the entire ML life-cycle for mission-critical and future-ready enterprises. 
+  The Future of Neural Networks, Harmonising Ai and Quantum Computing.
   `
   var obj_paratext = {
       name:  'Algoreus',
--- dist/dotHtml.js
+++ dist/dotHtml.js
@@ -1,13 +1,14 @@
 export function b1() {
     const divhtml = `
-        <p class="label_text1">AI AUTOMATION</p>
-        <p class="label_text2">
-        Enigma
-            <sup class="eni-sup">
-            2.0
-            </sup>
-        </p>
- 
+       <div class="label-header">
+            <p class="label_text1">AI AUTOMATION</p>
+            <p class="label_text2">
+            Enigma
+                <sup class="eni-sup">
+                2.0
+                </sup>
+            </p>
+        </div>
         <input type="checkbox" class="d_input1" id="input1" name="inputss"/>
         <label for="input1">
             <div id="b1" class="h_button"></div>
@@ -19,13 +20,15 @@ export function b1() {
 
  export function b2() {
     const divhtml = `
-        <p class="label_text1">DATA INTEGRATION</p>
-        <p class="label_text2">
-        Fabriq
-            <sup class="fab-sup">
-            n
-            </sup>
-        </p>
+        <div class="label-header">
+            <p class="label_text1">DATA INTEGRATION</p>
+            <p class="label_text2">
+            Fabriq
+                <sup class="fab-sup">
+                n
+                </sup>
+            </p>
+        </div>
         <input type="checkbox" class="d_input2" id="input2" name="inputss"/>
         <label for="input2">
             <div id="b2" class="h_button"></div>
@@ -37,8 +40,10 @@ export function b1() {
 
  export function b3() {
     const divhtml = `
-        <p class="label_text1">ML SPAWNER</p>
-        <p class="label_text2">Algoreus</p>
+        <div class="label-header">
+            <p class="label_text1">ML SPAWNER</p>
+            <p class="label_text2">Algoreus</p>
+        </div>
         <input type="checkbox" class="d_input3" id="input3" name="inputss"/>
         <label for="input3">
             <div id="b3" class="h_button"></div>
@@ -50,8 +55,10 @@ export function b1() {
 
  export function b4() {
     const divhtml = `
-        <p class="label_text1">ABOUT</p>
-        <p class="label_text2">TURIUM</p>
+        <div class="label-header">
+            <p class="label_text1">ABOUT</p>
+            <p class="label_text2">TURIUM</p>
+        </div>
         <input type="checkbox" class="d_input4" id="input4" name="inputss"/>
         <label for="input4">
             <div id="b4" class="h_button"></div>
